f2e76cbdaa768ea275b3b62795a0f750b5e0a66e
Harsh-Trivedi78 <trivediharsh346@gmail.com>
2025-05-19 17:48:01 +0530

    Add implementation for calculating GCD using recursion

---
+++ Basic_programs/GCDRecursive.java
@@ -0,0 +1,18 @@
+package Basic_programs;
+
+public class GCDRecursive {
+
+    public static int gcd(int a, int b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    public static void main(String[] args) {
+        int num1 = 48;
+        int num2 = 18;
+
+        int result = gcd(num1, num2);
+        System.out.println("GCD is: " + result);
+    }
+}
